5eff989e0458e5ae07134d15a67d94937bd36f78
Harshit <harshitjaintata@gmail.com>
2023-12-21 06:43:46 +0530

    Completed the Weather Web App
    Finally Created the weather web application

--- server.py
+++ server.py
@@ -8,7 +8,34 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    return render_template('index.html')
+
+@app.route('/weather')
+def get_weather():
+    city = request.args.get('city')
+    
+    # Check for empty strings or strings with spaces
+    if not bool(city.strip()):
+        city = "Jamshedpur"
+        
+        
+    weather_data = get_current_weather(city)
+    
+    #City not found
+    if not weather_data['cod'] == 200:
+        return render_template('city-not-found.html')
+    
+    
+    
+    return render_template(
+        "weather.html",
+        title = weather_data["name"],
+        status = weather_data["weather"][0]["description"].capitalize(),
+        temp = f"{weather_data['main']['temp']:.1f}",
+        feels_like = f"{weather_data['main']['feels_like']:.1f}",
+        humidity = f"{weather_data['main']['humidity']:.1f}",
+        pressure = f"{weather_data['main']['pressure']:.1f}"
+        )
 
 
 if __name__ == "__main__":
--- weather.py
+++ weather.py
@@ -6,7 +6,7 @@ import os
 load_dotenv()
 
 def get_current_weather(city="Jamshedpur"):
-    request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
+    request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric'
     
     weather_data = requests.get(request_url).json()
     
@@ -16,6 +16,10 @@ if __name__ == "__main__":
     print('\n*** Get Current Weather Conditions ***\n')
     
     city = input("\nPlease enter a city name: ")
+    
+    # Check for empty strings or strings with spaces
+    if not bool(city.strip()):
+        city = "Jamshedpur"
     
     weather_data = get_current_weather(city)
     
